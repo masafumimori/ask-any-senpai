47fdbb59a503395b07f6c418d51e0e8a13690aaa
masafumimori <m.masafumi@outlook.com>
2020-09-26 09:50:01 +0900

    Initial commit

---
+++ src/test/java/com/masafumimori/studyabroad/StudyabroadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.masafumimori.studyabroad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyabroadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
