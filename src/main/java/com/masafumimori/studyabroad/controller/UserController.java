99e011df7c5c6f6335f8fea01688ee6908ce3698
masafumimori <m.masafumi@outlook.com>
2020-09-28 11:41:52 +0900

    Added transition to login.html

--- src/main/java/com/masafumimori/studyabroad/controller/UserController.java
+++ src/main/java/com/masafumimori/studyabroad/controller/UserController.java
@@ -8,7 +8,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class UserController {
 
 	@RequestMapping("/register")
-	public String index() {
+	public String register() {
 		return "register";
+	}
+	
+	@RequestMapping("/login")
+	public String login() {
+		return "login";
 	}
 }
