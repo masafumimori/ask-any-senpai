9ae8c46a71d27c1063483c321a807343d9c6de10
masafumimori <m.masafumi@outlook.com>
2020-09-30 16:41:13 +0900

    Added code to store login session and show data on mypage

--- src/main/java/com/masafumimori/studyabroad/controller/UserController.java
+++ src/main/java/com/masafumimori/studyabroad/controller/UserController.java
@@ -3,6 +3,7 @@ package com.masafumimori.studyabroad.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -11,6 +12,7 @@ import com.google.gson.Gson;
 import com.masafumimori.studyabroad.model.domain.MstUser;
 import com.masafumimori.studyabroad.model.form.UserForm;
 import com.masafumimori.studyabroad.model.mapper.MstUserMapper;
+import com.masafumimori.studyabroad.model.session.LoginSession;
 
 @Controller
 @RequestMapping("/studyabroad/user")
@@ -18,6 +20,9 @@ public class UserController {
 	
 	@Autowired
 	MstUserMapper userMapper;
+	
+	@Autowired
+	LoginSession loginSession;
 	
 	private Gson gson = new Gson();
 	
@@ -50,11 +55,27 @@ public class UserController {
 		
 		MstUser user = userMapper.loginByUserNameAndPass(userName, password);
 		
+		if(user != null) {
+			loginSession.setUserId(user.getId());
+			loginSession.setUserName(user.getUserName());
+			loginSession.setPassword(user.getPassword());
+			loginSession.setLogined(true);
+		}
+		
 		return gson.toJson(user);
 	}
 	
 	@RequestMapping("/mypage")
-	public String mypage() {
+	public String mypage(Model m) {
+		
+		String userName = loginSession.getUserName();
+		String password = loginSession.getPassword();
+		
+		MstUser user = userMapper.loginByUserNameAndPass(userName, password);
+		
+		m.addAttribute("user", user);
+		//m.addAttribute("loginSession", loginSession);
+
 		return "mypage";
 	}
 	
