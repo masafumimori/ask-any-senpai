1091efafece667276d87e632ae397dd2c3124e3e
masafumimori <m.masafumi@outlook.com>
2020-09-30 16:40:27 +0900

    Created file for login session

---
+++ src/main/java/com/masafumimori/studyabroad/model/session/LoginSession.java
@@ -0,0 +1,53 @@
+package com.masafumimori.studyabroad.model.session;
+
+import java.io.Serializable;
+
+import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.ScopedProxyMode;
+import org.springframework.stereotype.Component;
+
+@Component
+@Scope(value="session", proxyMode=ScopedProxyMode.TARGET_CLASS)
+public class LoginSession implements Serializable {
+
+	private static final long serialVersionUID = 2776246967233145062L;
+	
+	private int userId;
+	private int tmpUserId;
+	private String userName;
+	private String password;
+	private boolean logined;
+	
+	public int getUserId() {
+		return userId;
+	}
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+	public int getTmpUserId() {
+		return tmpUserId;
+	}
+	public void setTmpUserId(int tmpUserId) {
+		this.tmpUserId = tmpUserId;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public boolean isLogined() {
+		return logined;
+	}
+	public void setLogined(boolean logined) {
+		this.logined = logined;
+	}
+	
+	
+}
