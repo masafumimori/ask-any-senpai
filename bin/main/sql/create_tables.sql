955fa6d56d57d3abd30f05b65088b83e804beb38
masafumimori <m.masafumi@outlook.com>
2020-10-02 08:05:55 +0900

    Created sql files

---
+++ bin/main/sql/create_tables.sql
@@ -0,0 +1,6 @@
+CREATE TABLE mst_user (
+    id int(11) PRIMARY KEY AUTO_INCREMENT,
+    user_name varchar 32 NOT NULL UNIQUE,
+    password varchar 16 NOT NULL,
+    email varchar 32 NOT NULL
+);
---
+++ src/main/resources/sql/create_database.sql
@@ -0,0 +1,2 @@
+DROP DATABASE IF EXISTS studyabroad;
+CREATE DATABASE studyabroad;
