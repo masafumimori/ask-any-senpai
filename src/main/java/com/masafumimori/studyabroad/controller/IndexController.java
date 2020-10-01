2fe569b13b067cbcc3678ffac61441601d17bd62
masafumimori <m.masafumi@outlook.com>
2020-10-01 17:19:33 +0900

    Added login session to switch buttons on the header

--- src/main/java/com/masafumimori/studyabroad/controller/IndexController.java
+++ src/main/java/com/masafumimori/studyabroad/controller/IndexController.java
@@ -1,17 +1,35 @@
 package com.masafumimori.studyabroad.controller;
 
+import com.masafumimori.studyabroad.model.domain.MstUser;
+import com.masafumimori.studyabroad.model.mapper.MstUserMapper;
+import com.masafumimori.studyabroad.model.session.LoginSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/studyabroad")
 public class IndexController {
 
+	@Autowired
+	LoginSession loginSession;
+
+	@Autowired
+	MstUserMapper userMapper;
+
 	@RequestMapping("/")
-	public String index() {
+	public String index(Model m) {
+
+		if (loginSession.isLogined()) {
+
+			MstUser user = userMapper.loginByUserNameAndPass(loginSession.getUserName(), loginSession.getPassword());
+
+			m.addAttribute("user", user);
+			//m.addAttribute("loginSession", loginSession);
+		}
 		return "index";
 	}
-	
-	
-}
 
+}
