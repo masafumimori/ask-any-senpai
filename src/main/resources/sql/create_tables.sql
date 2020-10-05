eef2650d0530a15b68bd7b9cceaf56190c702879
masafumimori <m.masafumi@outlook.com>
2020-10-05 14:10:14 +0900

    Added nations and areas columns

--- src/main/resources/sql/create_tables.sql
+++ src/main/resources/sql/create_tables.sql
@@ -2,5 +2,7 @@ CREATE TABLE mst_user (
     id int(11) PRIMARY KEY AUTO_INCREMENT,
     user_name varchar 32 NOT NULL UNIQUE,
     password varchar 16 NOT NULL,
-    email varchar 32 NOT NULL
+    email varchar 32 NOT NULL,
+    area varchar 50 NOT NULL,
+    nation varchar 100 NOT NULL
 );
