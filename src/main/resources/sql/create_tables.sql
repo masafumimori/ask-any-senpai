3c640674922235af7d302a8e0140c0887f52412f
masafumimori <m.masafumi@outlook.com>
2020-10-06 14:29:39 +0900

    Created: table for user SNS info

--- src/main/resources/sql/create_tables.sql
+++ src/main/resources/sql/create_tables.sql
@@ -1,12 +1,24 @@
 USE studyabroad;
 
 CREATE TABLE mst_user (
-    id INT(11) PRIMARY KEY AUTO_INCREMENT,
-    user_name VARCHAR(32) NOT NULL UNIQUE,
-    password VARCHAR(16) NOT NULL,
-    email VARCHAR(32) NOT NULL,
-    area VARCHAR(16) NOT NULL,
-    nation VARCHAR(16) NOT NULL,
-    created_at TIMESTAMP NOT NULL default now(),
-    updated_at TIMESTAMP NOT NULL default now() 
+	id INT(11) PRIMARY KEY AUTO_INCREMENT,
+	user_name VARCHAR(32) NOT NULL UNIQUE,
+	password VARCHAR(16) NOT NULL,
+	email VARCHAR(32) NOT NULL,
+	area VARCHAR(16) NOT NULL,
+	nation VARCHAR(16) NOT NULL,
+	created_at TIMESTAMP NOT NULL default now(),
+	updated_at TIMESTAMP NOT NULL default now() 
+);
+
+CREATE TABLE mst_user_sns (
+	id INT(11) PRIMARY KEY AUTO_INCREMENT,
+	user_id INT(11) NOT NULL,
+	user_name VARCHAR(32) NOT NULL UNIQUE,
+	twitter VARCHAR(50) NOT NULL,
+	instagram VARCHAR(50) NOT NULL,
+	facebook VARCHAR(50) NOT NULL,
+	created_at TIMESTAMP NOT NULL default now(),
+	updated_at TIMESTAMP NOT NULL default now(),
+	FOREIGN KEY(user_id) REFERENCES mst_user(id)
 );
