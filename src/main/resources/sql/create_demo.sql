f538e00806d74e0808cbfda0e1de0aedb58c8129
masafumimori <m.masafumi@outlook.com>
2020-10-06 16:56:55 +0900

    Added demo data

--- src/main/resources/sql/create_demo.sql
+++ src/main/resources/sql/create_demo.sql
@@ -21,10 +21,16 @@ VALUES
 -- User SNS demo
 INSERT INTO mst_user_sns
 	(
-	user_id, user_name, twitter, instagram, facebook
+		user_id, user_name, twitter, instagram, facebook
 	)
 VALUES
 	(
-		"1", "masa", "morimamama", "morimamama123", "XXXXX"
+		1, "masa", "morimamama", "morimamama123", "morima123"
+	),
+	(
+		2, "yamada", "yamada", "yamada123", "yamada123"
+	),
+	(
+		3, "michael", "michael", "michael123", "michael123"
 	)
 ;
