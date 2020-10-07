aba3afa638fa1d1761661053b7fe53f565b7dc77
masafumimori <m.masafumi@outlook.com>
2020-10-07 16:41:23 +0900

    Deleted not null on area and nation

--- src/main/resources/sql/create_tables.sql
+++ src/main/resources/sql/create_tables.sql
@@ -2,24 +2,42 @@ USE studyabroad;
 
 CREATE TABLE mst_user (
 	id INT(11) PRIMARY KEY AUTO_INCREMENT,
-	user_name VARCHAR(32) NOT NULL UNIQUE,
-	password VARCHAR(16) NOT NULL,
-	email VARCHAR(32) NOT NULL,
-	area VARCHAR(16) NOT NULL,
-	nation VARCHAR(16) NOT NULL,
-	created_at TIMESTAMP NOT NULL default now(),
-	updated_at TIMESTAMP NOT NULL default now() 
+	user_name VARCHAR
+(32) NOT NULL UNIQUE,
+	password VARCHAR
+(16) NOT NULL,
+	email VARCHAR
+(32) NOT NULL,
+	area VARCHAR
+(16),
+	nation VARCHAR
+(16),
+	created_at TIMESTAMP NOT NULL default now
+(),
+	updated_at TIMESTAMP NOT NULL default now
+() 
 );
 
 CREATE TABLE mst_user_sns (
 	id INT(11) PRIMARY KEY AUTO_INCREMENT,
-	user_id INT(11) NOT NULL,
-	user_name VARCHAR(32) NOT NULL UNIQUE,
-	twitter VARCHAR(50),
-	instagram VARCHAR(50),
-	facebook VARCHAR(50),
-	created_at TIMESTAMP NOT NULL default now(),
-	updated_at TIMESTAMP NOT NULL default now(),
-	FOREIGN KEY(user_id) REFERENCES mst_user(id),
-	FOREIGN KEY(user_name) REFERENCES mst_user(user_name)
+	user_id INT
+(11) NOT NULL,
+	user_name VARCHAR
+(32) NOT NULL UNIQUE,
+	twitter VARCHAR
+(50),
+	instagram VARCHAR
+(50),
+	facebook VARCHAR
+(50),
+	created_at TIMESTAMP NOT NULL default now
+(),
+	updated_at TIMESTAMP NOT NULL default now
+(),
+	FOREIGN KEY
+(user_id) REFERENCES mst_user
+(id),
+	FOREIGN KEY
+(user_name) REFERENCES mst_user
+(user_name)
 );
