b3d058ee51578a8ec1726324cbc06d5f489991ee
masafumimori <m.masafumi@outlook.com>
2020-10-09 17:09:52 +0900

    Added login session with search users

--- src/main/java/com/masafumimori/studyabroad/controller/UserSearchController.java
+++ src/main/java/com/masafumimori/studyabroad/controller/UserSearchController.java
@@ -9,46 +9,61 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.masafumimori.studyabroad.model.domain.dto.UserSearchDto;
 import com.masafumimori.studyabroad.model.form.SearchForm;
+import com.masafumimori.studyabroad.model.mapper.MstUserMapper;
 import com.masafumimori.studyabroad.model.mapper.UserSearchMapper;
+import com.masafumimori.studyabroad.model.session.LoginSession;
 
 @Controller
 @RequestMapping("/studyabroad/search")
 public class UserSearchController {
-	
+
 	@Autowired
 	UserSearchMapper searchMapper;
+	
+	@Autowired
+	MstUserMapper userMapper;
+
+	@Autowired
+	LoginSession loginSession;
 
 	@RequestMapping("/")
 	public String searchUser(SearchForm s, Model m) {
-		
+
 		List<UserSearchDto> users;
 		String[] areas;
 		String[] nations;
-		
-		//Need to modify below
+
+		// Need to modify below
 		String keywords = s.getKeywords();
 
-		if(s.getAreas() != null) {
+		if (s.getAreas() != null) {
 			areas = s.getAreas().split(",");
 		} else {
 			areas = null;
 		}
-		
-		if(s.getNations() != null) {
+
+		if (s.getNations() != null) {
 			nations = s.getNations().split(",");
 		} else {
 			nations = null;
 		}
-		
-		if(keywords == null) {
+
+		if (keywords == null) {
 			users = searchMapper.findByAreaAndNation(areas, nations);
 		} else {
-			//Code to search with keywords and areas/nations
+			// Code to search with keywords and areas/nations
 			users = null;
 		}
 		
+		String userName = loginSession.getUserName();
+		String password = loginSession.getPassword();
+
+		UserSearchDto user = userMapper.loginByUserNameAndPass(userName, password);
+
+		m.addAttribute("user", user);
+
 		m.addAttribute("users", users);
-		
+
 		return "index";
 	}
 }
