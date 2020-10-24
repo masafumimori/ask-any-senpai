5ad90f1d5b082dc1b0299cf952bf4c4a88a191d9
masafumimori <m.masafumi@outlook.com>
2020-10-24 20:31:30 +0900

    Created area-nation.js and moved script

---
+++ bin/main/static/js/area-nation.js
@@ -0,0 +1,35 @@
+//<![CDATA[
+    
+      const areas = {
+        asia: "アジア",
+        europe: "ヨーロッパ",
+        americaN: "北アメリカ",
+        americaS: "南アメリカ",
+        oceania: "オセアニア",
+        africa: "アフリカ",
+        others: "その他"
+      };
+
+      const nations = {
+        china: "中国",
+        korea: "韓国",
+        philippines: "フィリピン",
+        thailand: "タイ",
+        taiwan:"台湾",
+        vietnam: "ベトナム",
+        england: "イギリス",
+        australia: "オーストラリア",
+        newzealand: "ニュージーランド",
+        italy: "イタリア",
+        spain: "スペイン",
+        germany: "ドイツ",
+        france: "フランス",
+        portugal: "ポルトガル",
+        america: "アメリカ",
+        canada: "カナダ",
+        brazil: "ブラジル",
+        argentina: "アルゼンチン",
+        columbia: "コロンビア",
+        others: "その他"
+      };
+//]]>
