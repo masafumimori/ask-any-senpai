2f939370791640ceff8f9d21f1b8a895da895fb4
masafumimori <m.masafumi@outlook.com>
2020-10-28 16:17:41 +0900

    Added validation and error check when form submits

--- src/main/resources/static/js/validator.js
+++ src/main/resources/static/js/validator.js
@@ -11,11 +11,13 @@ $(() => {
             },
 
             password: {
-                required: true
+                required: true,
+                minlength: 4
             },
 
             rePassword: {
                 required: true,
+                minlength: 4,
                 equalTo: "#password"
             },
 
@@ -34,16 +36,18 @@ $(() => {
 
             userName: {
                 required: required_message,
-                minlength: "４文字以上でご入力ください。"
+                minlength: "4文字以上でご入力ください。"
             },
 
             password: {
-                required: required_message
+                required: required_message,
+                minlength: "4文字以上でご入力ください。"
             },
 
             rePassword: {
                 required: required_message,
-                equalTo: "上記のパスワードと異なります。再度ご入力ください。"
+                minlength: "4文字以上でご入力ください。",
+                equalTo: "パスワードが異なります。再度ご入力ください。"
             },
 
             email: {
@@ -54,9 +58,10 @@ $(() => {
             reEmail: {
                 required: required_message,
                 email: "正しいメールアドレスをご入力ください。",
-                equalTo: "上記のメールアドレスと異なります。再度ご入力ください。"
+                equalTo: "メールアドレスが異なります。再度ご入力ください。"
             } 
         },
+        
         // Change the place of error messages to after input-box
         errorPlacement: function(error, element){
             error.appendTo(element.parent().next(".error-messages"));
@@ -72,9 +77,9 @@ $(() => {
         },
 
         // Submit using ajax when form is valid
-        submitHandler: function(form){
-            $(form).ajaxSubmit();
-        },
+        // submitHandler: function(form){
+        //     $(form).ajaxSubmit();
+        // },
 
         // When inputs are invalid
         invalidHandler: function(event, validator){
